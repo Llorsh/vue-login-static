3643890b6067cdd5304bba5a5c20943a9bfb2699
Llorsh <jorge.m.arias.p@gmail.com>
2020-10-20 23:57:37 -0300

    20.10.2020 2357

---
+++ src/components/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="login-component">
+    <br />
+    <form method="POST" action="#" v-on:submit.prevent="onFormSend">
+      <input type="email" placeholder="Email" v-model="email" />
+      <br />
+      <br />
+      <input
+        type="password"
+        name="password"
+        id=""
+        placeholder="Password"
+        v-model="password"
+      />
+      <br />
+      <br />
+      <button type="submit">Entrar</button>
+    </form>
+
+    <br />
+    <br />
+    <br />
+    {{ credenciales.email }}
+    {{ credenciales.password }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login-component",
+  data() {
+    return {
+      credenciales: {
+        email: "email@email.cl",
+        password: "pass123",
+      },
+
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    onFormSend() {
+      console.log(this.email, this.password);
+      console.log(this.credenciales.email, this.credenciales.password);
+
+      if (this.email == this.credenciales.email) {
+        if (this.password == this.credenciales.password) {
+          alert("Bienvenido " + this.email);
+
+          setTimeout(() => {
+              window.location.href = '/principal'
+          }, 3000);
+        } else {
+          alert("Contraseña incorrecta");
+        }
+      } else {
+        alert("correo no valido");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
